96934ed87eb71690df206cc9ff13f81a57a0afe1
Daria Salova <pingvinchik41@gmail.com>
2023-07-23 16:28:06 +0300

    burger menu done

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,40 +1,54 @@
+import { useState } from 'react'
 import React from 'react'
 
 function Nav() {
+  const [visible, setVisible] = useState(true)
+  const toggleVisibility = () => setVisible(!visible)
+
+  function toggleVisibilityDown(e) {
+    if (e.keyCode === 13) {
+      toggleVisibility()
+    }
+  }
+
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <button className="nav__burger burger">
+      <button
+        tabIndex={0}
+        onClick={toggleVisibility}
+        onKeyDown={toggleVisibilityDown}
+        className="nav__burger burger"
+      >
         <span className="burger__line"></span>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
       </button>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      {visible && (
+        <div className="nav__menu menu">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Главное
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Мой плейлист
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Войти
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   )
 }
 
 export default Nav
-
-const burger__click = document.querySelector('nav__burger')
-
