0f9e654904941dbec0f71137c6c98f2d50fcecc6
Daria Salova <pingvinchik41@gmail.com>
2023-07-23 18:21:59 +0300

    create skelet

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import { useState } from 'react'
+import React from 'react'
 
-function Search() {
+function Search({ loading }) {
+  const [visibleFilter, setVisibleFilter] = useState(null)
+
+  const toggleVisibleFilter = (filter) => {
+    setVisibleFilter(visibleFilter === filter ? null : filter)
+  }
+
   return (
-  <>
-    <div className="centerblock__search search">
-      <svg className="search__svg">
-        <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-      </svg>
-      <input
-        className="search__text"
-        type="search"
-        placeholder="Поиск"
-        name="search"
-      />
-    </div>
-    <h2 className="centerblock__h2">Треки</h2>
-    <div className="centerblock__filter filter">
-      <div className="filter__title">Искать по:</div>
-      <div className="filter__button button-author _btn-text">исполнителю</div>
-      <div className="filter__button button-year _btn-text">году выпуска</div>
-      <div className="filter__button button-genre _btn-text">жанру</div>
-    </div>
-  </>
+    <>
+      <div className="centerblock__search search">
+        <svg className="search__svg">
+          <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+        </svg>
+        <input
+          className="search__text"
+          type="search"
+          placeholder="Поиск"
+          name="search"
+        />
+      </div>
+      <h2 className="centerblock__h2">Треки</h2>
+      <div className="centerblock__filter filter">
+        <div className="filter__title">Искать по:</div>
+        <button className="filter__button button-author _btn-text">
+          исполнителю
+        </button>
+        <button className="filter__button button-year _btn-text">году выпуска</button>
+        <button className="filter__button button-genre _btn-text">жанру</button>
+      </div>
+    </>
   )
 }
 
-export default Search;
+export default Search
---
+++ src/skelet/skelet.jsx
@@ -0,0 +1,7 @@
+import StyledSkelet from './skelet'
+
+function Skelet({ width, height }) {
+  return <StyledSkelet width={width} height={height} />
+}
+
+export default Skelet
