33b211c26eb787bf15977d2480e1bb218e4d5ae1
h0oliganka <pingvinchik41@gmail.com>
2023-07-26 17:04:25 +0300

    filter

--- src/components/filterAuthor.jsx
+++ src/components/filterAuthor.jsx
@@ -1,12 +1,12 @@
 function FilterAuthor() {
   return <div className="centerblock__filter_item_text">
-    <a className="filterAuthorLink" href="">Michael Jackson</a>
-    <a className="filterAuthorLink" href="">Nero</a>
-    <a className="filterAuthorLink" href="">Ali Bakgor</a>
-    <a className="filterAuthorLink" href="">Dynoro, Outwork, Mr. Gee</a>
-    <a className="filterAuthorLink" href="">minthaze</a>
-    <a className="filterAuthorLink" href="">Tom Boxer</a>
-    <a className="filterAuthorLink" href="">minthaze</a>
+    <a className="filtersButton" href="">Michael Jackson</a>
+    <a className="filtersButton" href="">Nero</a>
+    <a className="filtersButton" href="">Ali Bakgor</a>
+    <a className="filtersButton" href="">Dynoro, Outwork, Mr. Gee</a>
+    <a className="filtersButton" href="">minthaze</a>
+    <a className="filtersButton" href="">Tom Boxer</a>
+    <a className="filtersButton" href="">minthaze</a>
   </div>
 }
 
--- src/components/filterYear.jsx
+++ src/components/filterYear.jsx
@@ -1,12 +1,12 @@
 function FilterYear() {
   return <div className="centerblock__filter_item_text">
-    <a className="filterAuthorLink" href="">1987</a>
-    <a className="filterAuthorLink" href="">1990</a>
-    <a className="filterAuthorLink" href="">1995</a>
-    <a className="filterAuthorLink" href="">2001</a>
-    <a className="filterAuthorLink" href="">2005</a>
-    <a className="filterAuthorLink" href="">2015</a>
-    <a className="filterAuthorLink" href="">2022</a>
+    <a className="filtersButton" href="">1987</a>
+    <a className="filtersButton" href="">1990</a>
+    <a className="filtersButton" href="">1995</a>
+    <a className="filtersButton" href="">2001</a>
+    <a className="filtersButton" href="">2005</a>
+    <a className="filtersButton" href="">2015</a>
+    <a className="filtersButton" href="">2022</a>
   </div>
 }
 
