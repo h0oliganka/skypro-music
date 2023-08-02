ed1af688c9ab51026d538af106ddfa0dd2f5132f
Daria Salova <pingvinchik41@gmail.com>
2023-08-02 17:51:32 +0300

    loginStyled

--- src/pages/login/login.styled.js
+++ src/pages/login/login.styled.js
@@ -1,6 +1,10 @@
 import { styled } from 'styled-components'
 
 export const Login = styled.div`
-  display: flex;
-  width: 200px;
+  width: 250px;
+  height: 250px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -125px 0 0 -125px;
 `
