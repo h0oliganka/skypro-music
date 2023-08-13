52e1902df379ad341aeb047a68db8678bfcd71e7
h0oliganka <pingvinchik41@gmail.com>
2023-08-13 22:49:43 +0300

    routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,16 +9,45 @@ import { IndieCharge } from './pages/compilations/indieCharge'
 import { ProtectedRoute } from './pages/protected/protected'
 import { NotFound } from './pages/404/NotFound'
 
-export const AppRoutes = ({ user }) => {
+export const AppRoutes = () => {
+  let isLoggedIn = Boolean(localStorage.getItem('Token'));
+  if (isLoggedIn === true) {
+    <Main />
+  } else {
+    <Login />
+  }
+
   return (
     <Routes>
-      <Route element={<ProtectedRoute isAllowed={Boolean(localStorage.getItem('Token'))} />}>
-        <Route path="/favorites" element={<MyTracks />} />
-        <Route path="/category/1" element={<PlaylistOfTheDay />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/category/2" element={<DanceHits />} />
-        <Route path="//category/3" element={<IndieCharge />} />
-      </Route>
+      <Route path="/" element={
+        <ProtectedRoute isAllowed={isLoggedIn} redirectPath="/login">
+          <Main />
+        </ProtectedRoute>
+      }></Route>
+
+      <Route path="/favorites" element={
+        <ProtectedRoute isAllowed={isLoggedIn} redirectPath="/login">
+          <MyTracks />
+        </ProtectedRoute>
+      }></Route>
+
+      <Route path="/category/1" element={
+        <ProtectedRoute isAllowed={isLoggedIn} redirectPath="/login">
+          <PlaylistOfTheDay />
+        </ProtectedRoute>
+      }></Route>
+
+      <Route path="/category/2" element={
+        <ProtectedRoute isAllowed={isLoggedIn} redirectPath="/login">
+          <DanceHits />
+        </ProtectedRoute>
+      }></Route>
+
+      <Route path="/category/3" element={
+        <ProtectedRoute isAllowed={isLoggedIn} redirectPath="/login">
+          <IndieCharge />
+        </ProtectedRoute>
+      }></Route>
 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<RegisterUser />} />
